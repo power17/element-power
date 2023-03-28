d9aa5fd01288ef70cd4520135300673e21afd487
power <power@powerdeMacBook-Pro.local>
2023-03-28 21:51:28 +0800

    fix: 模块引入不对

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import ElContainer from './components/Container'
+import ElContainer from './components/Container/index'
 
 createApp(App)
 	.use(ElContainer)
