f9cc54556887e8d1e367f3db0ce1241428bf3533
power <power@powerdeMacBook-Pro.local>
2023-03-28 21:30:40 +0800

    build: 根据环境修改base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({command}) => {
+  let base = ''
+  base = command === 'build'? '/dist' : ''
+  console.log(base, command)
+  return {
+    base: base,
+    plugins: [vue()],
+  }
 })
