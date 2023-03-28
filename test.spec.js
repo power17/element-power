51d54c970f38163ece2d80d8dca53b5557801b20
power <power@powerdeMacBook-Pro.local>
2023-03-28 21:06:30 +0800

    test: add test case

--- test.spec.js
+++ test.spec.js
@@ -1,3 +1,14 @@
 test('test add', () => {
   expect(1 + 2).toBe(3)
 })
+function sayHello(name, fn) {
+  if (name == '大圣') {
+    fn()
+  }
+}
+test('测试函数', () => {
+  const fn = jest.fn()
+
+  sayHello('大圣', fn)
+  expect(fn).toHaveBeenCalled()
+})
