561d2d3311ef6dd67471b1627f639dbe1195b080
power <power@powerdeMacBook-Pro.local>
2023-03-29 20:24:32 +0800

    fix: path error in ubuntu

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,11 +1,11 @@
-import { App } from 'vue'
+// import { App } from 'vue'
 import ElContainer from './Container.vue'
 import ElHeader from './Header.vue'
 import ElFooter from './Footer.vue'
 import ElMain from './Main.vue'
 import ElAside from './Aside.vue'
 export default {
-	install(app: App) {
+	install(app) {
 		app.component(ElContainer.name, ElContainer)
 		app.component(ElHeader.name, ElHeader)
 		app.component(ElFooter.name,ElFooter)
