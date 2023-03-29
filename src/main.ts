3149da80f799d5930326ca39693d1d39e279ba0c
power <power@powerdeMacBook-Pro.local>
2023-03-29 20:26:19 +0800

    fix: path error in ubuntu1

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 
-import ElContainer from './components/Container'
+import ElContainer from './components/Container/index.js'
 
 createApp(App)
 	.use(ElContainer)
