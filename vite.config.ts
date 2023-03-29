15c6b148e99c7455c54ea160c9631b361855ba69
power <power@powerdeMacBook-Pro.local>
2023-03-29 21:21:14 +0800

    build: fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import rollupPluginNodeResolve from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   let base = ''
-  base = command === 'build' ? './dist' : './'
+  base = command === 'build' ? '/dist' : './'
   console.log(base, command)
   return {
     base: base,
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      rollupPluginNodeResolve({
+        extensions: ['.js', '.ts', 'tsx', '.vue']
+      })
+    ],
   }
 })
