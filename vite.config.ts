593e82ff60ee7543202bb54f802aeab36b5e2626
power <power@powerdeMacBook-Pro.local>
2023-04-15 13:41:13 +0800

    workflow: update

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  let base = ''
-  base = command === 'build' ? '/element' : './'
+  let base = command === 'build' ? '/element' : './'
   return {
     base,
     build: {
